15b084e48a3a2c7cc417a205a8c456b19768493f
Calcifer-02 <02.calcifer@gmail.com>
2025-11-05 12:13:37 +0300

    chore: scaffold monorepo structure and basic templates

---
+++ .eslintrc.cjs
@@ -0,0 +1,8 @@
+﻿module.exports = {
+  env: { browser: true, es2021: true, node: true },
+  extends: ["eslint:recommended", "plugin:react/recommended", "prettier"],
+  parser: "@babel/eslint-parser",
+  parserOptions: { ecmaVersion: "latest", sourceType: "module", requireConfigFile: false },
+  plugins: ["react"],
+  rules: {}
+};
